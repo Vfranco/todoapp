b50a5febfefe2cdde7afcc08efa0556e85dfbbd5
Damar452 <damarh452@gmail.com>
2021-07-20 13:03:07 -0500

    Actualizado-CorrecionBug

--- src/app/app.js
+++ src/app/app.js
@@ -4,13 +4,14 @@ var tareas = new Array();
 
 
 function agregar() {
+
     var entrada = document.getElementById('inputTareas');
     
     tareas.push(entrada.value);
 
     entrada.value = " ";
 
-    console.log(fruta);
+    console.log(tareas);
     
 }
 
