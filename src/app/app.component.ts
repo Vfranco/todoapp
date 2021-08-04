21599bc251451f72434ebbaf7d98ad4228f1d719
luisponton <luis0130@gmail.com>
2021-08-03 22:45:19 -0500

    M{etodo Editar Finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,56 @@
-import { Component } from '@angular/core';
+import { splitClasses } from '@angular/compiler';
+import { Component, enableProdMode } from '@angular/core';
 import { task } from './TasksList/interfaces/tareas.interfaces';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      tasksList:task[] = [];
-  taskName:string='';
-   
-  addTask():void {
-     if(this.taskName!=''){
-      let task:task={
-         dato:this.taskName
+export class AppComponent {
+  tasksList: task[] = [];
+  taskName: string = '';
+  isEditar: boolean = false;
+  indice:number=0;
+  tituloBoton:string='Guardar Tarea';
+  
+  addTask(): void {
+    if (!this.isEditar) {
+      this.tituloBoton='Guardar Tarea';
+      if (this.taskName != '') {
+            let task: task = {
+          dato: this.taskName,
+        };
+        this.tasksList.push(task);
+        this.taskName = '';
+      } else {
+        console.log('Dato no agregado');
+      }
+      this.isEditar=false;
+    }else{
+      let taskDelete: task = {
+        dato: this.taskName, //dato ingresado por el usuario por defecto espacio en blanco
       };
-       this.tasksList.push(task);
+       this.tasksList.splice(this.indice, 1, taskDelete);
+       this.isEditar=false;
+       this.tituloBoton='Guardar Tarea';
        this.taskName='';
-     }else{
-     console.log("Dato no agregado");
-   }
-   }//fin addTask
+       console.log(this.tasksList);
+    }
+  } //fin addTask
 
-   editTask(indice:number):void {
-    console.log("Editar");
-    
+  editTask(indice: number, nombre: string): void {
+    this.isEditar=true;
+    this.tituloBoton='Editar Tarea';
+    this.taskName = nombre;
+    this.indice=indice;
+    console.log(this.tasksList);
+  } //fin editTask
 
-  }//fin editTask
+  deleteTask(indice: number): void {
+    this.tasksList.splice(indice, 1);
+    console.log('Indice de Dato Eliminado' + ' ' + indice);
+    console.log(this.tasksList);
+  } //fin editTask
 
-  deleteTask(indice:number):void {
-     this.tasksList.splice(indice,1);
-    console.log("Indice de Dato Eliminado"+" "+indice);
-    console.log(this.tasksList);      
-  }//fin editTask
 }
-
